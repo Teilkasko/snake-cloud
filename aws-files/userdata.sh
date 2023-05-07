b9f5479cb566007196c761cf6c67648278921398
Teilkasko <teilkasko@online.de>
2023-05-07 22:20:22 +0200

    add aws files

--- aws-files/userdata.sh
+++ aws-files/userdata.sh
@@ -3,7 +3,7 @@
 # Userdata script to deploy the app on an AWS EC2 machine running Amazon Linux
 
 yum install -y git python3-pip
-pip3 install virtualenv
+sudo -u ec2-user pip3 install virtualenv
 
 cd /home/ec2-user
 git clone --branch develop https://github.com/fsolaroli/SnAkE.git snake
@@ -13,8 +13,6 @@ chown -R ec2-user:ec2-user .
 
 source ./bin/activate
 sudo -u ec2-user pip3 install -r requirements.txt
-cd src/server
-# This way the output of main.py will be written to log.txt
-# But, unfortunately, not before the process ends
-sudo -u ec2-user python3 main.py > log.txt &
-sudo -u ec2-user echo $! > pid.txt
+
+cp aws-files/startup.service /lib/systemd/system/startup.service
+systemctl enable startup.service --now
